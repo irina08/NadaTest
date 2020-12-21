8baad76c115cda5336b12b89e6fe45d4d730d609
irina08 <irina_ger@yahoo.com>
2020-12-21 00:52:36 -0800

    Initial commit

---
+++ src/test/java/com/HappyPathTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import com.pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class HappyPathTest {
+
+
+}
