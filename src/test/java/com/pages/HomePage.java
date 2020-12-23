844ceaac06529420060c61c937dcb88f1568d575
irina08 <irina_ger@yahoo.com>
2020-12-23 00:10:57 -0800

    added elements to HomePage.java

--- src/test/java/com/pages/HomePage.java
+++ src/test/java/com/pages/HomePage.java
@@ -1,9 +1,33 @@
 package com.pages;
 
 import com.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 
 public class HomePage extends Base
 {
+    private String title = "nada - Disposable Temp Email";
+    private static String URL = "https://getnada.com/";
+    private String domainName = "@getnada.com";
+
+    // elements
+    @FindBy(xpath = "//button[contains(text(), 'Add inboxe')]")
+    private WebElement addInboxe;
+
+    @FindBy(xpath = "//input[@id='grid-first-name']")
+    //input[#'grid-first-name'] input#grid-first-name //input[#'grid-first-name']
+    private WebElement userName;
+
+    //@FindBy(className = ".appearance-none.bg-white.block.border.border-gray-400.leading-tight.pr-8.px-2.py-2.rounded.shadow.w-full")
+    @FindBy(xpath = "//select")
+    private WebElement dropDownDomainName;
+
+    @FindBy(xpath = "//button[contains(text(), 'Add now!')]")
+    private WebElement generate_email;
+
+    @FindBy(xpath = "//p[@class='p-3']")
+    private WebElement confirmationText;
+
 
 }
