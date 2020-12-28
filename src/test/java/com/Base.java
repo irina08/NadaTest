c68fbe70aac9a657b5e90dd8c2a7d578367492a1
irina08 <irina_ger@yahoo.com>
2020-12-28 00:08:10 -0800

    modified Base.java

--- src/test/java/com/Base.java
+++ src/test/java/com/Base.java
@@ -9,12 +9,18 @@ public class Base
 {
     protected WebDriver driver = null;
 
+
     protected void clickElement(WebElement element)
     {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions
                 .elementToBeClickable(element))
                 .click();
+    }
+
+    protected void waitElementToBeVisible(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
 }
