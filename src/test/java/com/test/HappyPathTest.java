65078508c2e696e13a68c17e4b1c3f6c255de8f0
irina08 <irina_ger@yahoo.com>
2020-12-30 00:08:49 -0800

    added structure to HappyPathTest.java

--- src/test/java/com/test/HappyPathTest.java
+++ src/test/java/com/test/HappyPathTest.java
@@ -1,13 +1,44 @@
 package com.test;
 
-import org.junit.jupiter.api.*;
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HappyPathTest {
+
+    private WebDriver driver;
+    private String userEmail;
+    private String catUrl = "http://aws.random.cat/meow";
+    //private String dogUrl = "https://random.dog/woof.json";
+    private String foxUrl = "http://randomfox.ca/floof/";
+
+
+    @BeforeAll
+    static void oneTimeSetUp()
+    {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        driver = new ChromeDriver();
+    }
+
+
+	/*
+	    @AfterEach
+	    void tearDown()
+	    {
+	        driver.quit();
+	    }
+
+	 */
 
 
 }
